f9a848ef6765ee1fb9d9ef164ec4edcb472a83e7
quangminh <lequangminh9768@gmail.com>
2025-04-26 14:07:20 +0700

    Fix camera for live stream

--- streamClient.py
+++ streamClient.py
@@ -5,22 +5,24 @@ import socket
 import datetime
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 8889))
+client_socket.connect(('10.229.242.30', 55555))
 cap = cv2.VideoCapture(0)
 img_counter = 0
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 while True:
 	sendingTime = datetime.datetime.now()
 	ret, frame = cap.read()
+	cv2.imshow("P2P Live Stream (Client)", frame)
 	result, frame = cv2.imencode('.jpg', frame, encode_param)
 	dictionary = {
 		"frame": frame,
         "time": sendingTime
 	}
-
+	
 	data = pickle.dumps(dictionary, 0)
 	size = len(data)
-	print("{}: {}".format(img_counter, size))
+	if cv2.waitKey(1) == ord('q'):
+		break
 	client_socket.sendall(struct.pack(">L", size) + data)
 	img_counter += 1
 cap.release()
--- streamServer.py
+++ streamServer.py
@@ -6,47 +6,48 @@ import struct
 import datetime
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('127.0.0.1', 8889))
+serv.bind(('10.229.242.30', 55555))
 serv.listen(5)
 print("Ready to serve . . .")
 
 def startConnection(serv):
+    conn, addr = serv.accept()
+    data = b""
+    payload_size = struct.calcsize(">L")
+    print("payload_size: {}".format(payload_size))
 
-	conn, addr = serv.accept() 
-	data = b""
-	payload_size = struct.calcsize(">L")
-	print("payload_size: {}".format(payload_size))
-	while True:
-		while len(data) < payload_size:
-			data += conn.recv(4096)
-			if len(data)==0:
-				startConnection(serv)
-		#print("Done Recv: {}".format(len(data)))
-		packed_msg_size = data[:payload_size]
-		data = data[payload_size:]
-		msg_size = struct.unpack(">L", packed_msg_size)[0]
-		print("msg_size: {}".format(msg_size))
-		recivDate = datetime.datetime.now()
+    while True:
+        while len(data) < payload_size:
+            packet = conn.recv(4096)
+            if not packet:
+                print("Client disconnected.")
+                return
+            data += packet
 
-		while len(data) < msg_size:
-			container = conn.recv(4096)
-			if len(container)==0:
-				startConnection(serv)
-			data += container
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        print("msg_size: {}".format(msg_size))
 
-		frame_data = data[:msg_size]
-		data = data[msg_size:] 
+        while len(data) < msg_size:
+            packet = conn.recv(4096)
+            if not packet:
+                print("Incomplete frame received, skipping...")
+                break
+            data += packet
 
-		frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-		print("delay: {}".format(recivDate-frame['time']))
-		frame = cv2.imdecode(frame['frame'], cv2.IMREAD_COLOR)		
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
 
-		cv2.imshow('Streaming Video', frame)
+        frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        print("delay: {}".format(datetime.datetime.now() - frame['time']))
+        frame = cv2.imdecode(frame['frame'], cv2.IMREAD_COLOR)
 
-		cv2.waitKey(1)
+        cv2.imshow('Fetch Video', frame)
+        if cv2.waitKey(1):
+            break
 
-	print("Close the client socket")
-		
-	conn.close()
+    conn.close()
 
-startConnection(serv)
+while True:
+    startConnection(serv)
