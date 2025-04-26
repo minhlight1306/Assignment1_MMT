d21a47641dfbf6b3791f25709c25006110234ce2
quangminh <lequangminh9768@gmail.com>
2025-04-26 14:05:59 +0700

    Add function display online users

--- client.py
+++ client.py
@@ -15,36 +15,17 @@ WHITE = "white"
 FONT = ("Helvetica", 17)
 BUTTON_FONT = ("Helvetica", 15)
 SMALL_FONT = ("Helvetica", 13)
+GREAT_SMALL_FONT = ("Helvetica", 8)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = '10.0.113.167'
+HOST = '10.0.140.1'
 POST = 55555 # use any port from 0 to 65535
 
 def add_message(message):
     message_box.config(state=tk.NORMAL)
     message_box.insert(tk.END, message + '\n')
+    message_box.see(tk.END)
     message_box.config(state=tk.DISABLED)
-
-def connect():
-    # connect to server
-    username  = username_textbox.get().strip()
-    if(username == ""):
-        messagebox.showerror("Invalid username", "Username cannot be empty")
-        return
-    try:
-        client.connect((HOST, POST))
-        print(f"Successfully connect to server")
-        add_message("[SERVER] Successfully connected to the server")
-        client.sendall(username.encode())
-        username_textbox.config(state=tk.DISABLED)
-        username_button.config(state=tk.DISABLED)
-        stream_button.config(state=tk.NORMAL)
-
-        threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
-    except:
-        messagebox.showerror("Unable to connect to server", f"Unable to server {HOST} {POST}")
-        return
-
 
 def send_message():
     message = message_textbox.get()
@@ -54,25 +35,134 @@ def send_message():
     else:
         messagebox.showerror("Empty message", "Message cannot be empty")
 
-def client_stream(client):
-    cap = cv2.VideoCapture(0)
-    img_counter = 0
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-    while True:
-        sendingTime = datetime.datetime.now()
-        ret, frame = cap.read()
-        result, frame = cv2.imencode('.jpg', frame, encode_param)
-        dictionary = {
-            "frame": frame,
-            "time": sendingTime
-        }
+def register_peer(username):
+    message = f"REGISTER {username}"
 
-        data = pickle.dumps(dictionary, 0)
-        size = len(data)
-        print("{}: {}".format(img_counter, size))
-        client.sendall(struct.pack(">L", size) + data)
-        img_counter += 1
-    cap.release()
+    print(f"Register: {message}")
+    client.sendall(message.encode())
+
+def connect():
+    # connect to server
+    username  = username_textbox.get().strip()
+    if(username == ""):
+        messagebox.showerror("Invalid username", "Username cannot be empty")
+        return
+    try:
+        client.connect((HOST, POST))
+        
+        register_peer(username) # Register the peer with the server
+        username_textbox.config(state=tk.DISABLED)
+        username_button.config(state=tk.DISABLED)
+        stream_button.config(state=tk.NORMAL)
+        message_button.config(state=tk.NORMAL)
+
+        # Start a thread to listen for messages from the server
+        threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+    except:
+        messagebox.showerror("Unable to connect to server", f"Unable to server {HOST} {POST}")
+
+def send_request_live_stream(info):
+    while True:
+        try:
+            client.sendall("START_LIVE".encode())
+            print("start live")
+            while True:
+                try:
+                    response = client.recv(2048).decode("utf-8") # error here
+                    print(f"Received request: {response}")
+                    if response.startswith("READY~"):
+                            _, message = response.split("~")
+                            info[0], info[1], info[2] = message.split(":")
+                            print(f"Received live stream info: {message}")
+                            return
+                except Exception as e:
+                    print(f"Error : {e}") 
+                    return
+        except Exception as e:
+            print(f"Error send request: {e}")
+            return
+            
+def start_p2p_server():
+    print("Starting P2P server...")
+    info = []
+    send_request_live_stream(info)
+    print(f"Starting P2P server on ip {info[0]} listening port {info[1]} sending port {info[2]}")
+
+    # Cổng nghe
+    listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listening_socket.bind((info[0], int(info[1])))
+    listening_socket.listen(5)
+    print("Listening socket is running and waiting for connections...")
+
+    # Cổng gửi
+    sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sending_socket.connect((info[0], int(info[2])))
+    print("Connected to sending socket")
+
+    def handle_client(client_socket):
+        cap = cv2.VideoCapture(0)
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            cv2.imshow("Live Stream", frame)
+            result, encoded_frame = cv2.imencode('.jpg', frame, encode_param)
+            data = pickle.dumps(encoded_frame, 0)
+            size = len(data)
+
+            client_socket.sendall(struct.pack(">L", size) + data)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+            client_socket.sendall(struct.pack(">L", size) + data)
+
+        cap.release()
+        cv2.destroyAllWindows()
+        client_socket.close()
+
+    while True:
+        client_socket, addr = listening_socket.accept()
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
+
+# Function to live stream the video
+def connect_to_p2p_server(ip, port):
+    peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    peer_socket.connect((ip, port))
+    print(f"Connected to live stream at {ip}:{port}")
+
+    data = b""
+    payload_size = struct.calcsize(">L")
+
+    while True:
+        while len(data) < payload_size:
+            packet = peer_socket.recv(4096)
+            if not packet:
+                return
+            data += packet
+
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+        while len(data) < msg_size:
+            packet = peer_socket.recv(4096)
+            if not packet:
+                break
+            data += packet
+
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame['frame'], cv2.IMREAD_COLOR)
+
+        cv2.imshow("Fetch Live Stream", frame)
+        if cv2.waitKey(1):
+            break
+
+    peer_socket.close()
 
 root = tk.Tk()
 root.geometry("620x600")
@@ -101,7 +191,8 @@ username_textbox.pack(side=tk.LEFT)
 username_button = tk.Button(top_frame, text="Join", font=SMALL_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect)
 username_button.pack(side=tk.LEFT, padx=(15, 5))
 
-stream_button = tk.Button(top_frame, text="Live", font=SMALL_FONT, bg=OCEAN_BLUE, fg=WHITE, command=client_stream)
+stream_button = tk.Button(top_frame, text="Live", font=SMALL_FONT, bg=OCEAN_BLUE, fg=WHITE,
+                          command=start_p2p_server)
 stream_button.pack(side=tk.LEFT, padx=5)
 
 message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=38)
@@ -110,25 +201,77 @@ message_textbox.pack(side=tk.LEFT, padx=10)
 message_button = tk.Button(bottom_frame, text="Send", font=MEDIUM_GREY, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
 message_button.pack(side=tk.LEFT, padx=10)
 
-message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=26)
+message_box = tk.Text(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=62, height=26, wrap=tk.WORD)
 message_box.config(state=tk.DISABLED)
-message_box.pack(side=tk.TOP)
+message_box.pack(side=tk.LEFT, fill=tk.BOTH)
 
+online_box = tk.Text(middle_frame, font=GREAT_SMALL_FONT, bg=DARK_GREY, fg=WHITE, width=10, height=26, wrap=tk.NONE)
+online_box.config(state=tk.DISABLED)
+online_box.pack(side=tk.RIGHT, fill=tk.BOTH)
 
+def update_online_users(user):
+    online_box.config(state=tk.NORMAL)
+    online_box.insert(tk.END, f"{user}\n")
+    online_box.see(tk.END)
+    online_box.config(state=tk.DISABLED)
+
+def update_offline_users(user):
+    online_box.config(state=tk.NORMAL)
+    lines = online_box.get(1.0, tk.END).splitlines()  # Lấy tất cả các dòng trong online_box
+    online_box.delete(1.0, tk.END)  # Xóa toàn bộ nội dung
+    for line in lines:
+        if line.strip() != user:  # Chỉ thêm lại các user không phải là user cần xóa
+            online_box.insert(tk.END, f"{line}\n")
+    online_box.config(state=tk.DISABLED)
 
 def listen_for_messages_from_server(client):
     while True:
-        message = client.recv(2048).decode("utf-8")
-        if message != '':
-            username = message.split('~')[0]
-            content = message.split('~')[1]
+        try:
+            message = client.recv(2048).decode("utf-8")
+            print(f"Listen message: {message}")
+            if message != '':
+                if message.startswith("ONLINE_USERS~"):
+                    user = message.split('~')[1]
+                    print(user)
+                    update_online_users(user)
+                    client.sendall("ACK".encode())
 
-            add_message(f"[{username}] {content}")
-        else:
-            messagebox.showerror("Error", "Message received from client is empty")
+                elif message.startswith("ADD_ONLINE_USERS~"):
+                    user = message.split("~")[1]
+                    update_online_users(user)
+
+                elif message.startswith("OFFLINE_USERS~"):
+                    user = message.split('~')[1]
+                    update_offline_users(user)
+
+                elif message.startswith("LIVE_STREAM~"):
+                    ip, port = message.split("~")[1].split(":")
+                    threading.Thread(target=connect_to_p2p_server, args=(ip, int(port))).start()
+                
+                elif message.startswith("HISTORY"):
+                    fulcontent = ' '.join(message.split(' ')[1:])
+                    username = fulcontent.split('~')[0]
+                    content = fulcontent.split('~')[1]
+                    add_message(f"[{username}] {content}")
+                    client.sendall("ACK".encode())
+                    
+                else:
+                    username = message.split('~')[0]
+                    content = message.split('~')[1]
+                    add_message(f"[{username}] {content}")
+            else:
+                messagebox.showerror("Error", "Message received from client is empty")
+        except ConnectionResetError:
+            print("Connection to the server was lost.")
+            messagebox.showerror("Error", "Connection to the server was lost.")
+            break
+        except Exception as e:
+            print(f"Error listen message: {e}")
+            break
 
 def main():
     stream_button.config(state=tk.DISABLED)
+    message_button.config(state=tk.DISABLED)
     root.mainloop()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import threading
 import cv2
@@ -5,94 +6,149 @@ import pickle
 import numpy as np
 import struct
 import datetime
+import time
 
 
-HOST = '10.0.113.167'
+HOST = '10.0.140.1'
 POST = 55555 # use any port from 0 to 65535
 LISTENER_LIMIT = 5
 active_clients = []
 nicknames = []
+message_history = []
+peers = {}  # {username: (socket, (ip, port))}
+live_stream_info = None  # (username, ip, port)
 
 # Function to listen for upcoming mess from a client
-def listen_for_messages(client, username):
+def listen_for_messages(client, username, address):
     while True:
         try:
             message = client.recv(2048).decode("utf-8")
-            if message != '':
+            print(f"Listen message : {message}")
+            if not message:
+                print(f"Listen for message failed from {username}")
+                continue
+            if message.startswith("START_LIVE"):
+                break
+            if message != "ACK":
                 final_msg = username + '~' + message
                 send_message_to_all(final_msg)
-            else:
-                print(f"The message send from client {username} is empty")
+
         except ConnectionResetError:
-            print(f"Client {username} has disconnected from the server")
+            disconnect_message = f"{username} has disconnected from the server!"
+            print(f"{username} on {address[0], address[1]} has disconnected from the server!")
+            # Thông báo khi người dùng ngắt kết nối
+            if client in active_clients:
+                active_clients.remove(client)
+            if username in peers:
+                del peers[username]
+            # if live_stream_info and live_stream_info[0] == username:
+            #     live_stream_info = None
+            if username in nicknames:
+                send_message_to_all(f"OFFLINE_USERS~{username}")
+                nicknames.remove(username)
+            client.close()
+            send_message_to_all("SERVER~" + disconnect_message)
+            send_online_users(client)
             break
         except Exception as e:
-            print(f"Error receiving message from {username}: {e}")
-            break
-    if username in nicknames:
-        nicknames.remove(username)
-    if client in active_clients:        
-        active_clients.remove(client)
+            print(f"Error receiving message from {username}: {e}") 
 
 def send_message_to_client(client, message):
     client.sendall(message.encode())
 
 # Function to send any new mess to all the client connected to this clients
 def send_message_to_all(message):
+    if not message.startswith("ADD_ONLINE_USERS"):
+        message_history.append(message)  # Lưu tin nhắn vào lịch sử
     for user in active_clients:
         send_message_to_client(user, message)
 
-# Function to live stream the video
-def live_stream(client):
-    data = b""
-    payload_size = struct.calcsize(">L")
-    while True:
-        while len(data) < payload_size:
-            data += client.recv(4096)
-            if not data:
-                live_stream(client)
+def send_message_history(client):
+    for message in message_history:
+        while True:
+            send_message_to_client(client, "HISTORY " + message)  # Gửi lịch sử tin nhắn
+            ack = client.recv(1024).decode()         # Chờ xác nhận từ client
+            if ack == "ACK":
+                break  # Nếu nhận được ACK, tiếp tục với tin nhắn tiếp theo
+            else:
+                print("Resending message:", message)  # Nếu không nhận được ACK, gửi lại
 
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack(">L", packed_msg_size)[0]
-        print("msg_size: {}".format(msg_size))
-        recivDate = datetime.datetime.now()
+def send_online_users(client):
+    for user in nicknames:
+        while True:
+            send_message_to_client(client, "ONLINE_USERS~" + user)
+            ack = client.recv(1024).decode()
+            if ack == "ACK":
+                break
+            else:
+                print("Resending user:", user)
 
-        while len(data) < msg_size:
-            container = client.recv(4096)
-            if not container:
-                live_stream(client)
-            data += container
-
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-        print("delay: {}".format(recivDate - frame['time']))
-        frame = cv2.imdecode(frame['frame'], cv2.IMREAD_COLOR)
-
-        cv2.imshow("Live Stream", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
 # Function tp handle client
-def handle(client):
+def handle(client, addr):
+    global peers, live_stream_info
+    is_live = False
     while True:
-        message = client.recv(2048).decode("utf-8")
-        if message != '':
-            if message in active_clients:
-                print(f"Client {message} is already connected to server")
-                client.close()
-                break
+        try:
+            message = client.recv(2048).decode("utf-8")
+            print(f"Handle message from {addr}: {message}")
+            if not message:
+                continue
+            if message.startswith("REGISTER"):
+                username = message.split()[1]
+                if username in nicknames:
+                    print(f"Client {username} is already connected to server")
+                    client.close()
+                    break
+                peers[username] = (client, addr) #save socket and address
+                
+                nicknames.append(username)
+                if len(active_clients) > 0:
+                        send_message_to_all(f"ADD_ONLINE_USERS~{username}")
+                        time.sleep(1)
 
-            active_clients.append(client)
-            nicknames.append(message)
-            prompt_message = "SERVER~" + f"{message} has joined the server!"
-            send_message_to_all(prompt_message)
-            break
-        # live stream
-        # create a button to start live
+                active_clients.append(client)
+                # Gửi lịch sử tin nhắn cho người dùng mới
+                send_message_history(client)
 
-    threading.Thread(target=listen_for_messages, args=(client, message)).start()
+                # Thông báo người dùng mới tham gia
+                prompt_message = f"SERVER~{username} has joined the server!"
+                send_message_to_all(prompt_message)
+                time.sleep(1)
+                # Gửi danh sách người dùng đang online
+                send_online_users(client)
+                listen_for_messages(client, username, addr)  # Listen for messages from the client
+
+                # threading.Thread(target=listen_for_messages, args=(client, usermane)).start()
+                
+            elif message.startswith("START_LIVE"):
+                if live_stream_info is not None:
+                        send_message_to_client(client, "ERROR~Another live stream is already active.")
+                        continue
+                
+                listening_port = addr[1] + 1
+                sending_port = addr[1] + 2
+                live_stream_info = (username, addr[0], listening_port, sending_port)
+                print(f"{username} started live streaming on ports {listening_port} (listen) and {sending_port} (send)")
+                send_message_to_client(client, f"READY~{addr[0]}:{listening_port}:{sending_port}")
+
+            elif message == "BEGIN":
+                if username.size() <= 1:
+                    continue
+                for peer_username, (peer_socket, peer_addr) in peers.items():
+                    if peer_username != username:
+                        try:
+                            send_message_to_client(peer_socket, f"LIVE_STREAM~{addr[0]}:{live_stream_info[2]}:{live_stream_info[3]}")
+                        except Exception as e:
+                            print(f"Error sending live stream info to {peer_username}: {e}")
+                    # break
+            else:
+                if message == "ACK":
+                    send_message_to_client(client, "ACK")
+                listen_for_messages(client, username, addr)
+        except:
+            pass
+
 
 def main():
     # create a socket class object
@@ -112,7 +168,7 @@ def main():
         client, address = server.accept()
         print(f"Successfully connected with {address[0], address[1]}")
 
-        threading.Thread(target=handle, args=(client, )).start()
+        threading.Thread(target=handle, args=(client, address)).start()
 
 if __name__ == '__main__':
     main()
